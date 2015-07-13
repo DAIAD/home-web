ad11d6325487d90941e8126e480c666d5fbe183a
Yannis Kouvaras <yannis.kouvaras@kupa.gr>
2015-07-13 19:04:21 +0300

    Updated error handling for HTTP status codes 403 and 404.

--- src/main/java/eu/daiad/web/controller/ErrorController.java
+++ src/main/java/eu/daiad/web/controller/ErrorController.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.boot.autoconfigure.web.BasicErrorController;
 import org.springframework.boot.autoconfigure.web.ErrorAttributes;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -17,10 +18,32 @@ public class ErrorController extends BasicErrorController {
 		super(errorAttributes);
 	}
 
+	private HttpStatus getStatus(HttpServletRequest request) {
+		Integer statusCode = (Integer) request
+				.getAttribute("javax.servlet.error.status_code");
+		if (statusCode != null) {
+			try {
+				return HttpStatus.valueOf(statusCode);
+			}
+			catch (Exception ex) {
+			}
+		}
+		return HttpStatus.INTERNAL_SERVER_ERROR;
+	}
+	
 	@Override
 	@RequestMapping(value = "${error.path:/error}", produces = "text/html")
 	public ModelAndView errorHtml(HttpServletRequest request) {
-		return super.errorHtml(request);
+		HttpStatus status = this.getStatus(request);
+		
+		switch(status) {
+			case NOT_FOUND:
+				return new ModelAndView("notfound");
+			case FORBIDDEN:
+				return new ModelAndView("denied");
+			default:
+				return new ModelAndView("error");
+		}
 	}
 
 	@Override
