e973db1f12171283215e773dba672a7e227e684f
Stelios Manousopoulos <smanousopoulos@gmail.com>
2016-02-25 14:15:40 +0200

    home: separated login/logout form

--- src/main/resources/public/assets/js/src/home/components/LoginForm.js
+++ src/main/resources/public/assets/js/src/home/components/LoginForm.js
@@ -2,75 +2,77 @@
 var React = require('react');
 var injectIntl = require('react-intl').injectIntl;
 var FormattedMessage = require('react-intl').FormattedMessage;
-var Modal = require('react-bootstrap').Modal;
-var Button = require('react-bootstrap').Button;
-var connect = require('react-redux').connect;
+var classNames = require('classnames');
 
 
-
-
-var LoginForm = React.createClass({
+var Login = React.createClass({
 	onLogin: function(e) {
 		e.preventDefault();
 		this.props.onLogin(this.refs.username.value, this.refs.password.value);
 
 	},
-	onLogout: function(e) {
-		e.preventDefault();
-		this.props.onLogout();
-
-	},
-
 	render: function() {
 		var _t = this.props.intl.formatMessage;
 		var component;
 		if(this.props.isAuthenticated) {
-			component = (
-				<div key="logout"	className="">
-			   		<button id="logout"
-			   				type="submit"
-			   				className="btn btn-primary"
-	   						style={{ width: 80, height: 33 }}
-		   					onClick={this.onLogout}>
-			   			<FormattedMessage id="loginForm.button.signout" />
-   					</button>
-		   		</div>
-	   		);
-		} else {
-			component = (
-				<form key="login" className="form-login" action={this.props.action}>
-					<div className="form-group">
-						<input id="username" name="username" type="text" ref="username"
-							placeholder={_t({ id: 'loginForm.placehoder.username'})} className="form-control" />
-					</div>
-					<div className="form-group" >
-						<input id="password" name="password" type="password" ref="password"
-							placeholder={_t({ id: 'loginForm.placehoder.password'})} className="form-control" />
-					</div>
-					<button type="submit"
-							className="btn btn-primary action-login"
-							onClick={this.onLogin}
-							style={{width: 80, height: 33}}>
-						<FormattedMessage id="loginForm.button.signin" />
-					</button>
-					<br/>
-					<div className="login-errors">
-						{
-							this.props.errors?(<FormattedMessage id={"errors."+this.props.errors} />):(<div/>)
-					}
-				</div>
-				</form>
-			);
+			return (<div/>);
 		}
-
 		return (
-			<div className="login-out">
-				{component}
-   			</div>
+			<form key="login" className="form-login" action={this.props.action}>
+				<div className="form-group">
+					<input id="username" name="username" type="text" ref="username"
+						placeholder={_t({ id: 'loginForm.placehoder.username'})} className="form-control" />
+				</div>
+				<div className="form-group" >
+					<input id="password" name="password" type="password" ref="password"
+						placeholder={_t({ id: 'loginForm.placehoder.password'})} className="form-control" />
+				</div>
+				<button type="submit"
+						className="btn btn-primary action-login"
+						onClick={this.onLogin}
+						style={{width: 80, height: 33}}>
+					<FormattedMessage id="loginForm.button.signin" />
+				</button>
+				<br/>
+				<div className="login-errors">
+					{
+						this.props.errors?(<FormattedMessage id={"errors."+this.props.errors} />):(<div/>)
+				}
+			</div>
+		</form>
 		);
-	},
+	}
 
 });
 
-LoginForm = injectIntl(LoginForm);
-module.exports = LoginForm;
+
+var Logout = React.createClass({
+	
+	onLogout: function(e) {
+		e.preventDefault();
+		this.props.onLogout();
+	},
+	render: function() {
+		var _t = this.props.intl.formatMessage;
+		if(!this.props.isAuthenticated) {
+			return (<div/>);
+		}
+		return (
+			<a id="logout"
+				className="logout"
+				title={_t({id: "loginForm.button.signout"})}
+				onClick={this.onLogout}
+				type="submit">
+					<i className={classNames("fa", "fa-lg", "fa-sign-out", "navy")} />
+				</a>
+			);
+		} 
+});
+
+Login = injectIntl(Login);
+Logout = injectIntl(Logout);
+
+module.exports = {
+	Login,
+	Logout
+};
