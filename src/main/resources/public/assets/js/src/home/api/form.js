f3291177fce7022fc04178ea58874c1505a0b861
Stelios Manousopoulos <smanousopoulos@gmail.com>
2016-04-22 19:52:30 +0300

    home: fixed user login/logout issues with fetch

--- src/main/resources/public/assets/js/src/home/api/form.js
+++ src/main/resources/public/assets/js/src/home/api/form.js
@@ -1,29 +1,26 @@
 var fetch = require('isomorphic-fetch');
 require('es6-promise').polyfill();
 
-var formAPI = function(url, formData, method="POST") {
+var formAPI = function(url, data, method="POST") {
   
-  const { data, csrf } = formData;
+  const { formData, csrf } = data; 
 
   let fetchObj = {
     method: method,
     credentials: 'same-origin',
     headers: {
       'Accept': "application/json",
-      //'Content-Type': "application/json",
-      //'Content-Type': "application/x-www-form-urlencoded; charset=UTF-8",
+      'X-Requested-With':'XMLHttpRequest',
       'X-CSRF-TOKEN': csrf
     },
-    body: data 
+    body: formData 
   };
 
   return fetch(url, fetchObj) 
-    .then(response => { console.log('got response', response); return response; })
-    //.then(response => (response.status >= 200 && response.status < 300)?response:new Error(response.statusText))
-    //.then(response => response.text())
-    //.then(text => { console.log('got text', text); return text; })
-    .then(response => response.json().then(json => Object.assign({}, json, {csrf:response.headers.get('X-CSRF-TOKEN')})))
-    .then(response => { console.log('got json response', response); return response; });
+  .then(response => response.json()
+                    .then(json => Object.assign({}, json, {csrf:response.headers.get('X-CSRF-TOKEN')})))
+  .catch(error => { throw new Error('response parsing failed', error); });
+  
 };
 
 module.exports = formAPI;
