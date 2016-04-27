92cd091922e7b4cf154acae49fdea0950109c4b4
nkaragiannakis <karagiannakis.nk@gmail.com>
2016-04-27 15:09:12 +0300

    remove isRunning method

--- src/main/java/eu/daiad/web/service/IAggregatesService.java
+++ src/main/java/eu/daiad/web/service/IAggregatesService.java
@@ -14,7 +14,6 @@ public interface IAggregatesService {
     
     public abstract MessageAggregatesContainer execute(MessageCalculationConfiguration config);
 
-    public boolean isRunning();
     public void cancel();
     public boolean isCancelled();
 }
