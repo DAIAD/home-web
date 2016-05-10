7fbd5d48c38c167734a3178d9ef54c755f0d243d
nkaragiannakis <karagiannakis.nk@gmail.com>
2016-05-10 12:46:07 +0300

    update actions in reducer

--- src/main/resources/public/assets/js/src/utility/reducers/alerts.js
+++ src/main/resources/public/assets/js/src/utility/reducers/alerts.js
@@ -2,19 +2,37 @@ var types = require('../constants/ActionTypes');
 
 var initialState = {
   isLoading : false,
-  group : null
+  utility : null,
+  utilities : null,
+  tips : null
 };
 
 var alerts = function(state, action) {
-  switch (action.type) {
-    case types.ALERTS_GROUP_SELECTED:
-      return Object.assign({}, state, {
-        group : action.group
-      });
+    switch (action.type) {
+        case types.ALERTS_UTILITY_SELECTED:
+            console.log('reducer: ALERTS_UTILITY_SELECTED');  
+            return Object.assign({}, state, { 
+                isLoading : true,
+                utility : action.utility
+            });
 
-    default:
-      return state || initialState;
-  }
+        case types.ADMIN_REQUESTED_TIPS:
+            console.log('reducer: ADMIN_REQUESTED_TIPS');
+            return Object.assign({}, state, { 
+                isLoading : true,
+                tips : null
+            });
+
+        case types.ADMIN_RECEIVED_TIPS:
+            console.log('reducer: ADMIN_RECEIVED_TIPS');
+            return Object.assign({}, state, { 
+                isLoading : false,
+                tips : action.tips
+            });
+
+        default:
+          return state || initialState;
+    }
 };
 
 module.exports = alerts;
