1b0da31ccd5f52b08bf804e522e0899cb600a3f2
nkaragiannakis <karagiannakis.nk@gmail.com>
2016-05-12 17:31:23 +0300

    update getTips api

--- src/main/resources/public/assets/js/src/utility/api/alerts.js
+++ src/main/resources/public/assets/js/src/utility/api/alerts.js
@@ -1,20 +1,11 @@
 var api = require('./base');
 
 var ManageAlertsAPI = {
-    fetchTips: function(filter) {
-        return api.json('/action/profile/tips/list', filter);
-    },
-
-    fetchFilterOptions: function(){
-            return api.json('/action/profile/modes/filter/options');
-    },
     getAllUtilities: function(){
-        console.log('api: getAllUtilities');
         return api.json('/action/utility/fetch/all');
     },
-    getTips: function(){
-        console.log('api: getTips');
-        return api.json('/action/recommendation/static/en');
+    getTips: function(locale){
+        return api.json('/action/recommendation/static/' + locale);
     }        
 };
 
