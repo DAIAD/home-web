ea5a17e3ad57997600350f1a8f7ba1247bb76f16
nkaragiannakis <karagiannakis.nk@gmail.com>
2016-05-12 17:30:18 +0300

    add utilities request, utilities response and fetchUtilities actions

--- src/main/resources/public/assets/js/src/utility/actions/ManageAlertsActions.js
+++ src/main/resources/public/assets/js/src/utility/actions/ManageAlertsActions.js
@@ -1,40 +1,13 @@
-var queryAPI = require('../api/query');
+//var queryAPI = require('../api/query');
 var types = require('../constants/ActionTypes');
-var adminAPI = require('../api/admin');
+//var adminAPI = require('../api/admin');
 var alertsAPI = require('../api/alerts');
-var api = require('../api/base');
-
-var getUtilitiesRequest = function(){
-    console.log('3 (ManageAlertsActions)IN getUtilitiesRequest');
-    return {
-        type : types.GET_UTILITIES_REQUEST
-    };
-};
-
-var getUtilitiesResponse = function(success, utilities, errors){
-    
-    console.log('6 (ManageAlertsActions) getUtilitiesResponse, utilities ' + Object.keys(utilities) + ", success " + success + ", errors " + errors);
-    for(var obj in utilities){
-        if(utilities.hasOwnProperty(obj)){
-            for(var prop in utilities[obj]){
-                if(utilities[obj].hasOwnProperty(prop)){
-                    console.log(prop + ':' + utilities[obj][prop]);
-                }
-            }
-        }    
-    }
-    return{
-        type : types.ALERTS_GET_UTILITIES_RECEIVE_RESPONSE,
-        success : success,
-        utilities : utilities,
-        errors : errors
-    };
-};
+//var api = require('../api/base');
 
 var receivedTips = function(success, errors, tips) {
   console.log('action: receivedTips');  
   return {
-    type : types.ADMIN_RECEIVED_TIPS,
+    type : types.ADMIN_RECEIVED_STATIC_TIPS,
     success : success,
     errors : errors,
     tips : tips
@@ -44,49 +17,68 @@ var receivedTips = function(success, errors, tips) {
 var requestedTips = function(locale) {
    console.log('action: requestedTips');
   return {
-    type : types.ADMIN_REQUESTED_TIPS,
+    type : types.ADMIN_REQUESTED_STATIC_TIPS,
     locale : locale
+  };
+};
+
+var requestedUtilities = function() {
+   console.log('action: requestedUtilities');
+  return {
+    type : types.ADMIN_REQUESTED_UTILITIES,
+  };
+};
+
+var receivedUtilities = function(success, errors, utilities) {
+  return {
+    type : types.ADMIN_RECEIVED_UTILITIES,
+    success : success,
+    errors : errors,
+    utilities : utilities
   };
 };
  
 var ManageAlertsActions = {
     setUtility: function(event, utility) {
-        console.log('(ManageAlertsActions) IN setUtility');
         return{
-          type : types.ALERTS_UTILITY_SELECTED,
+          type : types.ADMIN_SELECTED_UTILITY_FILTER,
           utility : utility
         }; 
     }, 
-    getUtilities : function(){
-      
-        console.log('2 (ManageAlertsActions)IN getUtilities');
-        return alertsAPI.getAllUtilities().then(function(response){
-
-            console.log('5 (ManageAlertsActions) IN getAllUtilities');
-            getUtilitiesResponse(response.success, response.utilitiesInfo, response.errors);
-        }, function(error) {  
-            console.log('(ManageAlertsActions) IN getAllUtilities error ');
-            addUserReceiveResponse(false, null, error);
-        });                    
-    },
-    getStaticTips: function(event, utility) {
-    
-    console.log('action: getStaticTips');
-        //TODO - resolve locale for utility
-        var locale = "en";    
-        if(utility.label == "DAIAD"){
-            locale = "en";
-        }
-
-        return function(dispatch, getState) {
-          dispatch(requestedTips(locale));
-
-          return alertsAPI.getTips().then(function(response) {
-            dispatch(receivedTips(response.success, response.errors, response.recommendations));
-          }, function(error) {
-            dispatch(receivedTips(false, error, null));
+    fetchUtilities : function(event){
+      return function(dispatch, getState) {
+        dispatch(requestedUtilities());
+        return alertsAPI.getAllUtilities().then(function(response) {                  
+            dispatch(receivedUtilities(response.success, response.errors, response.utilitiesInfo));
+        }, function(error) {receivedUtilities(false, error, null);
           });
-        };                   
+      };
+    },
+    getStaticTips: function(event, utility) {   
+      console.log('action: getStaticTips');
+      var locale;    
+      if(utility.label == "DAIAD"){
+        locale = "en";
+      }
+      else if(utility.label == "Alicante"){
+        locale = "es";
+      }
+      else{
+        locale = "en";
+      }
+      return function(dispatch, getState) {
+        dispatch(requestedTips(locale));
+
+        return alertsAPI.getTips(locale).then(function(response) {
+
+          for (var key in response){
+              console.log("response["+ key +"]="+ response[key]);
+          }
+          dispatch(receivedTips(response.success, response.errors, response.messages));
+        }, function(error) {
+          dispatch(receivedTips(false, error, null));
+        });
+      };                   
     }
 };
 
