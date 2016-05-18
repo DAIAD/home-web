580f0c023f0e93eab3b27f51e411e76256431160
nkaragiannakis <karagiannakis.nk@gmail.com>
2016-05-18 17:13:48 +0300

    add edit/activate/de-activate methods for tips

--- src/main/java/eu/daiad/web/controller/action/RecommendationController.java
+++ src/main/java/eu/daiad/web/controller/action/RecommendationController.java
@@ -19,8 +19,12 @@ import eu.daiad.web.model.error.ApplicationException;
 import eu.daiad.web.model.message.EnumMessageType;
 import eu.daiad.web.model.message.Message;
 import eu.daiad.web.model.message.MessageResponse;
+import eu.daiad.web.model.message.StaticRecommendation;
+import eu.daiad.web.model.query.DataQueryRequest;
 import eu.daiad.web.model.security.AuthenticatedUser;
 import eu.daiad.web.repository.application.IMessageRepository;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @RestController
 public class RecommendationController extends BaseController {
@@ -84,6 +88,57 @@ public class RecommendationController extends BaseController {
 		}
 
 		return recommendations;
+	}
+        
+        @RequestMapping(value = "/action/recommendation/static/edit/{id}/{locale}/{imagePath}", 
+                method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
+        @ResponseBody
+        @Secured("ROLE_ADMIN")
+	public RestResponse editAdvisoryMessage(@AuthenticationPrincipal AuthenticatedUser user, 
+                @PathVariable String id, @PathVariable String locale, @PathVariable String imagePath) {
+		MessageResponse recommendations = new MessageResponse();
+
+		try {
+			//recommendations.setMessages(this.messageRepository.getAdvisoryMessages(locale));
+		} catch (ApplicationException ex) {
+			if (!ex.isLogged()) {
+				logger.error(ex.getMessage(), ex);
+			}
+
+			recommendations.add(this.getError(ex));
+		}
+
+		return null;
+	}
+        
+        @RequestMapping(value = "/action/recommendation/static/save/{locale}", 
+                method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
+        @Secured("ROLE_ADMIN")
+	public RestResponse setActiveAdvisoryMessage(@AuthenticationPrincipal AuthenticatedUser user, 
+                @RequestBody List<StaticRecommendation> data, @PathVariable String locale) {
+            
+            
+		RestResponse setActiveResponse = new MessageResponse();
+                
+
+                
+		try {
+                   
+                    
+                    for(StaticRecommendation d: data){
+                        System.out.println("success? " + d.getIndex() + " locale " + locale + " active:" + d.isActive());
+                        this.messageRepository.persistActiveAdvisoryMessage(locale, d.getIndex(), d.isActive());
+                    }                    
+    
+		} catch (ApplicationException ex) {
+			if (!ex.isLogged()) {
+				logger.error(ex.getMessage(), ex);
+			}
+
+			setActiveResponse.add(this.getError(ex));
+		}
+
+		return setActiveResponse;
 	}
 
 }
