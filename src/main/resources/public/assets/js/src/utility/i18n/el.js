523066be95c88b4c2bc90d586af03809b5bfb049
Yannis Kouvaras <yannis.kouvaras@kupa.gr>
2016-06-05 21:29:16 +0300

    Add session expiration detection at the client

--- src/main/resources/public/assets/js/src/utility/i18n/el.js
+++ src/main/resources/public/assets/js/src/utility/i18n/el.js
@@ -230,6 +230,7 @@
 		"403" : "Η πιστοποίηση απέτυχε",
 		"404" : "Not found",
 		"500" : "Internal server error",
+		"SharedErrorCode.SESSION_EXPIRED": "Η συνεδρία έχει λήξει.",
     "ValidationError.User.NO_FIRST_NAME": "Λείπει το Όνομα.",
     "ValidationError.User.NO_LAST_NAME": "Λείπει το Επώνυμο.",
     "ValidationError.User.NO_EMAIL": "Λείπει το E-mail.",
