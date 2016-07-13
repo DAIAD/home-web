dd6d91191e0b87255ea7b9623767ddefc95cd31f
Yannis Kouvaras <yannis.kouvaras@kupa.gr>
2016-07-13 13:36:53 +0300

    Update code for analysis algorithms

--- jobs/model-change/ann_script.R
+++ jobs/model-change/ann_script.R
@@ -1,7 +1,38 @@
 require(cpm)
 require(neuralnet)
 
-#performs the change-point detection test for the baselines
+get_data = function(path){
+  f     = file(path,open="r")
+  lines = readLines(f) 
+  close(f)
+  data = list()
+  for(i in 1:(length(lines)/3)){
+    index = (i-1)*3
+    data[[i]] = list()
+    data[[i]]$id     = strsplit(lines[index+1]," ")[[1]][1]
+    data[[i]]$cons   = as.numeric(strsplit(lines[index+2]," ")[[1]])
+    data[[i]]$dates  = as.Date(strsplit(lines[index+3]," ")[[1]])
+  }
+  return(data)
+}
+
+make_all_dataset_truth = function(user,hour,end=360){
+  x = t(matrix(user$cons[1:(end*24)],nrow=24))
+  d = make_dates(as.factor(weekdays(user$dates)[1:end]))
+  y = rowSums(t(matrix(user$cons[25:((end+1)*24)],nrow=24)))
+  f = data.frame(cbind(x,d,y))
+  #f= cbind(x,y)
+  colnames(f) = c(make.names(1:31),"y")
+  return(f)
+}
+
+make_dates = function(dates){
+  date_matrix = matrix(0,length(dates),7)
+  for(i in 1:length(dates))
+    date_matrix[i,dates[i]]=1
+  return(date_matrix)
+}
+
 run_cpd = function(x,ind=0){
   res = processStream(x,cpmType="Mann-Whitney")
   plot(x,xlab="day",ylab="error")
@@ -11,59 +42,74 @@ run_cpd = function(x,ind=0){
   return(list(res$changePoints,res$detectionTimes,x))
 }
 
-#trains from 'start' to 'end' and measures error from 'end' to 'end'+'test_points'
-test = function(d,start,end,test_points,perm=FALSE,w){
+monthly_filter = function(user){
+  len = floor(length(user$cons)/24/30)*30*24
+  if(len>10000)
+    return(FALSE)
+  d = t(matrix(user$cons[1:len],nrow=24*30))
+  month_cons = rowSums(d)
+  # plot(month_cons)
+  if(sum(sort(month_cons,decreasing=TRUE)[1:6])>0.8*sum(month_cons))
+    return(FALSE)
+  else
+    return(TRUE)
+}
+
+train_test_24 = function(d,start,end,test_points,perm=FALSE,w){
   if(perm!=FALSE)
     d = d[perm,]
   mses = c()
   test_start = end-test_points+1
   n = names(d)
   f = as.formula(paste('y ~', paste(n[!n %in% 'y'], collapse = ' + ')))
-  mdl  = neuralnet(f,d[start:test_start,],hidden=5,threshold=0.01,rep=1)
+  mdl  = neuralnet(f,d[start:test_start,],hidden=5,threshold=0.05,rep=1)
   #mdl = lm(y~.,data=d[start:(test_start-1),])  
-  res = compute(mdl,d[test_start:end,1:100])
-  ae = abs(d[test_start:end,101]-res$net.result)
+  res = compute(mdl,d[test_start:end,1:31])
+  ae = abs(d[test_start:end,32]-res$net.result)
   return(ae)
 }
-#adds 1 point to the training set and measures the validation error, by calling train_test. (Algorithm 1)
-seq_tt_d = function(d,t0,min_size,test_size,perm=FALSE){
+
+seq_tt_24_d = function(d,t0,min_size,test_size,perm=FALSE){
   ers = c()
   points = seq(t0-min_size,1,-1)
   for(i in points){
-    ers = c(ers, train_test(d,i,t0,test_size,perm))
+    ers = c(ers, train_test_24(d,i,t0,test_size,perm))
   }
   return(list(t(matrix(ers,nrow=test_size)),points))
 }
 
-get_train_res = function(d,t0){
+seq_tt_24 = function(user,h2,t0,min_size,test_size,perm=FALSE){
+  d = make_all_dataset_truth(user,h2)
+  d = d/max(d)
+  return(seq_tt_24_d(d,t0,min_size,test_size,perm))
+}
+
+get_train_res = function(user,h2,t0){
+  d = make_all_dataset_truth(user,h2)
   n = names(d)
   f = as.formula(paste('y ~', paste(n[!n %in% 'y'], collapse = ' + ')))
-  mdl  = neuralnet(f,data=d[1:t0,],hidden=5,threshold=0.01)
+  s = max(d[1:t0,])
+  d = d/s
+  mdl  = neuralnet(f,data=d[1:t0,],hidden=5,threshold=0.05,rep=1)
   #mdl = lm(y~.,data=d[start:(test_start-1),])  
-  pred = compute(mdl,d[1:t0,1:100])$net.result
-  res = d[1:t0,101] - pred
+  pred = compute(mdl,d[1:t0,1:31])$net.result*s
+  res = d[1:t0,32] - pred
   return(res)
 }
 
-#selects the optimal point for the start of the training set (Algorithm 1)
-select_start = function(d,t0,min_size=40,test_size=15,window=20,thresh=0.05,type="stat"){
+select_start_24 = function(user,h2,t0,min_size=40,test_size=15,window=20,thresh=0.05){
   
-  #seq_tt_d performs the iterating training-validating process of Algorithm 1
-  ers = seq_tt_d(d,t0,min_size,test_size,FALSE)
+  ers = seq_tt_24(user,h2,t0,min_size,test_size,FALSE)
   fin_er = ers[[1]][dim(ers[[1]])[1],]
   min_mid = min_moving_median(ers[[1]],window)
   min_mid_er = ers[[1]][min_mid[[1]],]
   
-  #the condition that evaluates whether the decrease in the error is significan (Algorithm 2 line 6)
-  if( type=="stat" && t.test(fin_er-min_mid_er)$p.value < thresh && mean(fin_er-min_mid_er)>0 ) 
-    sel1 = ers[[2]][min_mid[[1]]]
-  else if ( type=="emp" &&  mean(fin_er-min_mid_er)/mean(fin_er) > thresh )
+  if( t.test(fin_er-min_mid_er)$p.value<thresh && mean(fin_er-min_mid_er)>0) 
     sel1 = ers[[2]][min_mid[[1]]]
   else
     sel1 = 1
   
-  #runs change-point detection on the training residuals
-  d1 = get_train_res(d,t0)
+  d1 = get_train_res(user,h2,t0)
   cp = run_cpd(d1)
   
   if(length(cp[[1]])<1 )
@@ -73,8 +119,7 @@ select_start = function(d,t0,min_size=40,test_size=15,window=20,thresh=0.05,type
   if(t0-sel2<20 && length(cp[[1]])>1)
     sel2 = cp[[1]][length(cp[[1]])-1]
   
-  #runs change-point detection on the time series
-  d2 = d[1:t0,101]
+  d2 = make_all_dataset_truth(user)[1:t0,32]
   cp = run_cpd(d2)
   
   if(length(cp[[1]])<1 )
@@ -91,43 +136,43 @@ select_start = function(d,t0,min_size=40,test_size=15,window=20,thresh=0.05,type
   
 }
 
-#method that runs our algorithm and the baselines and measures the errors
-compare_methods = function(d,t0,min_size,test_size,ahead,window,thresh,type="stat"){
+compare_methods = function(user,h2,t0,min_size,test_size,ahead,window,thresh){
   
+  d = make_all_dataset_truth(user,h2)
   plot(d$y)
   abline(v=t0)
-  
-  y = d[,101]
+  if(any(d$y>10000)||length(d$y)>10000)
+    return(-d1)
+  y = d$y
   n = names(d)
   f = as.formula(paste('y ~', paste(n[!n %in% 'y'], collapse = ' + ')))
   s = max(d[1:t0,])
   d = d/s
   
-  mdl0  = neuralnet(f,d[1:t0,],hidden=5,threshold=0.01)
-  pred0 = compute(mdl0,d[(t0+1):(t0+ahead),1:100])$net.result*s
+  mdl0  = neuralnet(f,d[1:t0,],hidden=5,threshold=0.05,rep=1)
+  pred0 = compute(mdl0,d[(t0+1):(t0+ahead),1:31])$net.result*s
   er0   = mean(abs(y[(t0+1):(t0+ahead)] - pred0))
   
-  #calls select_start to determine the starts of the training sets
-  sel = select_start(d,t0,min_size,test_size,window,thresh,type)
+  sel = select_start_24(user,h2,t0,min_size,test_size,window,thresh)
   
-  mdl1  = neuralnet(f,d[sel[[1]]:t0,],hidden=5,threshold=0.01)
-  pred1 = compute(mdl1,d[(t0+1):(t0+ahead),1:100])$net.result*s
+  mdl1  = neuralnet(f,d[sel[[1]]:t0,],hidden=5,threshold=0.05,rep=1)
+  pred1 = compute(mdl1,d[(t0+1):(t0+ahead),1:31])$net.result*s
   
   if(sel[[1]]==1)
     er1=er0
   else
     er1   = mean(abs(y[(t0+1):(t0+ahead)] - pred1))
   
-  mdl2  = neuralnet(f,d[sel[[2]]:t0,],hidden=5,threshold=0.01)
-  pred2 = compute(mdl2,d[(t0+1):(t0+ahead),1:100])$net.result*s
+  mdl2  = neuralnet(f,d[sel[[2]]:t0,],hidden=5,threshold=0.05,rep=1)
+  pred2 = compute(mdl2,d[(t0+1):(t0+ahead),1:31])$net.result*s
   
   if(sel[[2]]==1)
     er2 = er0
   else
     er2 = mean(abs(y[(t0+1):(t0+ahead)] - pred2))
   
-  mdl3  = neuralnet(f,d[sel[[3]]:t0,],hidden=5,threshold=0.01)
-  pred3 = compute(mdl3,d[(t0+1):(t0+ahead),1:100])$net.result*s
+  mdl3  = neuralnet(f,d[sel[[3]]:t0,],hidden=5,threshold=0.05,rep=1)
+  pred3 = compute(mdl3,d[(t0+1):(t0+ahead),1:31])$net.result*s
   
   if(sel[[3]]==1)
     er3=er0
@@ -137,10 +182,40 @@ compare_methods = function(d,t0,min_size,test_size,ahead,window,thresh,type="sta
   pred4 = mean(y[sel[[3]]:t0])
   er4 = mean(abs(y[(t0+1):(t0+ahead)] - pred4))
   
-  return(c(er0,er1,er3,er2))
+  return(c(er0,er1,er2,er3,er4))
   
 }
 
+compare_many = function(start,end,min_size,test_size,ahead,window,thresh,path){
+  tried = c()
+  ers   = c()
+  users = get_data(path)
+  for(u in start:end){
+    if(monthly_filter(users[[u]]))
+      for( t in c(80,120,160,190,200,230,240,230,270,280,310,320,340)){
+        
+        part = c()
+        #h = sample(6:24,1)
+        
+        print(paste(u,t,sep=" "))
+        res = tryCatch(compare_methods(users[[u]],h,t,min_size,test_size,ahead,window,thresh),error=function(err){
+          print(err)
+          return(-1)
+        })
+        
+        if(res==-1)
+          next
+        
+        tried = c(tried,u,t)
+        ers   = c(ers,res)
+        print(res)
+        
+      }
+  }
+  
+  res = list(matrix(ers,nrow=5),matrix(tried,nrow=2))
+  save(res,file=paste("res-",min_size,test_size,ahead,window,thresh,".RData",sep=""))
+}
 
 min_moving_median = function(matrix,w){
   min = Inf
@@ -156,41 +231,32 @@ min_moving_median = function(matrix,w){
   return(list(index,min))
 }
 
-#method that performs the test
-test=function(data,min_train_size=10,val_size=25,points_ahead=10,window=10,thresh=0.1,type='stat'){
-  ers = c()
-  times = c()
-  for(i in 1:length(synthetic_data)){
-    print(i)
-    d = data[[i]]
-    for(t in c(80,120,160,200,240,280,320)){
-      res = tryCatch(compare_methods(d,t,min_train_size,val_size,points_ahead,window,thresh,type),error=function(err){
-        return(-1)
-      })
-      if (length(res)==1)
-        next
-      print(res)
-      ers = c(ers, res)
-      times = c(times , t)
+parse_results = function(res,t0){
+  hours = c()
+  ids = c()
+  for(i in 1:dim(res[[1]])[2]){
+    if(res[[2]][2,i]==t0){
+      hours = c(hours,res[[1]][,i])
+      ids = c(ids,res[[2]][,i] )
     }
   }
-  results = list(matrix(ers,nrow=4),times)
-  print(parse_all(results))
-  return(results)
+  ers = matrix(hours,nrow=5)
+  ids = matrix(ids, nrow=2)
+  r = c()
+  r = c(r,mean((ers[1,]-ers[2,])/ers[1,]))
+  r = c(r,mean((ers[1,]-ers[3,])/ers[1,]))
+  r = c(r,mean((ers[1,]-ers[4,])/ers[1,]))
+  r = c(r,mean((ers[1,]-ers[5,])/ers[1,]))
+  
+  return(r)
 }
 
 parse_all = function(res){
-  ers = matrix(0,3,8)
-  times = c(80,120,160,200,240,280,320)
-  for( i in 1:7){
-    for(j in 1:3){
-      r = res[[1]][,res[[2]]==times[i]]
-      ers[j,i] = mean(r[1,]-r[(j+1),])/mean(r[1,])
-    }
-  }
-  for(i in 1:3)
-    ers[i,8] = mean(ers[i,1:7])
-  
-  return(ers*100)
+  ers = array(dim=c(4,8))
+  t0 = c(80,120,160,200,240,280,320,340)
+  for( j in t0  )
+    ers[,which(t0==j)] = parse_results(res,j)
+  return(ers)  
 }
+
 
--- jobs/model-change/enet_script.R
+++ jobs/model-change/enet_script.R
@@ -1,22 +1,87 @@
 require(glmnet)
 require(cpm)
 
-#performs the change-point detection test for the baselines
+get_data = function(path){
+  f     = file(path,open="r")
+  lines = readLines(f) 
+  close(f)
+  data = list()
+  for(i in 1:(length(lines)/3)){
+    index = (i-1)*3
+    data[[i]] = list()
+    data[[i]]$id     = strsplit(lines[index+1]," ")[[1]][1]
+    data[[i]]$cons   = as.numeric(strsplit(lines[index+2]," ")[[1]])
+    data[[i]]$dates  = as.Date(strsplit(lines[index+3]," ")[[1]])
+  }
+  return(data)
+}
+
+make_all_dataset_truth = function(user,hour,end=360){
+  x = t(matrix(user$cons[1:(end*24)],nrow=24))
+  d = make_dates(as.factor(weekdays(user$dates)[1:end]))
+  y = rowSums(t(matrix(user$cons[25:((end+1)*24)],nrow=24)))
+  f = data.frame(cbind(x,d,y))
+  #f= cbind(x,y)
+  colnames(f) = c(make.names(1:31),"y")
+  return(f)
+}
+
+make_dates = function(dates){
+  date_matrix = matrix(0,length(dates),7)
+  for(i in 1:length(dates))
+    date_matrix[i,dates[i]]=1
+  return(date_matrix)
+}
+
 run_cpd = function(x,ind=0){
-  res = processStream(x,cpmType="Mann-Whitney")
-  plot(x,xlab="day",ylab="error")
-  abline(v=res$changePoints,col='red')
-  abline(v=res$detectionTimes,col='red',lty=2)
+  res = processStream(x,cpmType="Mann-Whitney",ARL0=500)
+  plot(x,xlab="Day",ylab="Consumption (Litres)")
+  abline(v=res$changePoints,col='red',lty=2)
+ # abline(v=res$detectionTimes,col='red',lty=2)
   # title(ind)
   return(list(res$changePoints,res$detectionTimes,x))
 }
 
-#trains from 'start' to 'end' and measures error from 'end' to 'end'+'test_points'
-train_test = function(d,start,end,test_points,perm=FALSE){
+monthly_filter = function(user){
+  len = floor(length(user$cons)/24/30)*30*24
+  if(len>10000)
+    return(FALSE)
+  d = t(matrix(user$cons[1:len],nrow=24*30))
+  month_cons = rowSums(d)
+  # plot(month_cons)
+  if(sum(sort(month_cons,decreasing=TRUE)[1:6])>0.8*sum(month_cons))
+    return(FALSE)
+  else
+    return(TRUE)
+}
+
+lambda_cv = function(x,y,k=5){
+  len       = length(y)
+  fold_size =  floor(len/k)
+  ers = c()
+  lam = glmnet(x,y,nlambda=20,alpha=0.5)$lambda
+  for(i in 0:(k-1)){
+    test_ind = (i*fold_size+1):(i*fold_size+fold_size)
+    train_x  = x[-test_ind,]
+    train_y  = y[-test_ind]
+    test_x   = x[test_ind,]
+    test_y   = y[test_ind]
+    mdl  = glmnet(train_x,train_y,lambda=lam,alpha=0.5)
+    pred = predict(mdl,test_x,nlambda=lam)
+    buf  = sqrt(colSums((pred-test_y)**2)/fold_size)
+    if(length(buf)==length(lam))
+      ers = c(ers,buf)
+  }
+  er_mat = matrix(ers,nrow=length(lam))
+  sums   = rowSums(er_mat)
+  return(mdl$lambda[which(sums==min(sums))])
+}
+
+train_test_24 = function(d,start,end,test_points,perm=FALSE){
   if(perm!=FALSE)
     d = d[perm,]
-  x = d[,1:100]
-  y = d[,101] 
+  x = d[,1:31]
+  y = d[,32] 
   mses = c()
   test_start = end-test_points+1
   lam  = lambda_cv(x[start:(test_start-1),],y[start:(test_start-1)])
@@ -26,79 +91,79 @@ train_test = function(d,start,end,test_points,perm=FALSE){
   return(ae)
 }
 
-#adds 1 point to the training set and measures the validation error, by calling train_test. (Algorithm 1)
-seq_tt_d = function(d,t0,min_size,test_size,perm=FALSE){
+seq_tt_24_d = function(d,t0,min_size,test_size,perm=FALSE){
   ers = c()
   points = seq(t0-min_size,1,-1)
   for(i in points){
-    ers = c(ers, train_test(d,i,t0,test_size,perm))
+    ers = c(ers, train_test_24(d,i,t0,test_size,perm))
   }
   return(list(t(matrix(ers,nrow=test_size)),points))
 }
 
+seq_tt_24 = function(user,h2,t0,min_size,test_size,perm=FALSE){
+  d = data.matrix(make_all_dataset_truth(user,h2))
+  return(seq_tt_24_d(d,t0,min_size,test_size,perm))
+}
+
 get_train_res = function(user,h2,t0){
   d = data.matrix(make_all_dataset_truth(user,h2))
-  x = d[,1:100]
-  y = d[,101] 
+  x = d[,1:31]
+  y = d[,32] 
   lam0 = lambda_cv(x[1:t0,],y[1:t0])
+ # lam0 = 0
   mdl0 = glmnet(x[1:t0,],y[1:t0],lambda=c(lam0),alpha=0.5)
   pred0 = predict(mdl0,x[1:t0,])
   res = y[1:t0] - pred0
   return(res)
 }
 
-#selects the optimal point for the start of the training set (Algorithm 1)
-select_start = function(d,t0,min_size=40,test_size=15,window=20,thresh=0.05,type="stat"){
+select_start_24 = function(user,h2,t0,min_size=40,test_size=15,window=20,thresh=0.05){
   
-  #seq_tt_d performs the iterating training-validating process of Algorithm 1
-  ers = seq_tt_d(d,t0,min_size,test_size,FALSE)
+  #thresh = sort(test_var(user,h2,t0,min_size-test_size,test_size))[25]
+  ers = seq_tt_24(user,h2,t0,min_size,test_size,FALSE)
   fin_er = ers[[1]][dim(ers[[1]])[1],]
   min_mid = min_moving_median(ers[[1]],window)
   min_mid_er = ers[[1]][min_mid[[1]],]
   
-  #the condition that evaluates whether the decrease in the error is significan (Algorithm 2 line 6)
-  if( type=="stat" && t.test(fin_er-min_mid_er)$p.value < thresh && mean(fin_er-min_mid_er)>0 ) 
-    sel1 = ers[[2]][min_mid[[1]]]
-  else if ( type=="emp" &&  mean(fin_er-min_mid_er)/mean(fin_er) > thresh )
+  if( t.test(fin_er-min_mid_er)$p.value<thresh && mean(fin_er-min_mid_er)>0) 
     sel1 = ers[[2]][min_mid[[1]]]
   else
     sel1 = 1
   
-  #runs change-point detection on the training residuals
-  d1 = get_train_res(d,t0)
+  d1 = get_train_res(user,h2,t0)
   cp = run_cpd(d1)
   
   if(length(cp[[1]])<1 )
     sel2 = 1
   else
     sel2 = cp[[1]][length(cp[[1]])]
-  
   if(t0-sel2<20 && length(cp[[1]])>1)
-    sel2 = cp[[1]][length(cp[[1]])-1]
+    sel3 = cp[[1]][length(cp[[1]])-1]
   
-  #runs change-point detection on the time series
-  cp = run_cpd(d[1:t0,101])
+  
+  d2 = make_all_dataset_truth(user)[1:t0,32]
+  cp = run_cpd(d2)
   
   if(length(cp[[1]])<1 )
     sel3 = 1
   else
     sel3 = cp[[1]][length(cp[[1]])]
-  
   if(t0-sel3<20 && length(cp[[1]])>1)
     sel3 = cp[[1]][length(cp[[1]])-1]
   
-  plot(ers[[2]],rowSums(abs(ers[[1]]))/test_size)
+  
+  plot(ers[[2]],rowSums(ers[[1]])/test_size)
   abline(v=c(sel1,sel2,sel3),col=c(2,3,4))
   
   return(list(sel1,sel2,sel3))
-  
+
 }
 
-#method that runs our algorithm and the baselines and measures the errors
-compare_methods = function(d,t0,min_size,test_size,ahead,window,thresh,type){
-  
-  x = d[,1:100]
-  y = d[,101] 
+compare_methods = function(user,h2,t0,min_size,test_size,ahead,window,thresh){
+
+  d = data.matrix(make_all_dataset_truth(user,h2))
+  x = d[,1:31]
+  y = d[,32] 
   plot(y)
   abline(v=t0)
   if(any(y>10000)||length(y)>10000)
@@ -108,12 +173,7 @@ compare_methods = function(d,t0,min_size,test_size,ahead,window,thresh,type){
   pred0 = predict(mdl0,x[(t0+1):(t0+ahead),])
   er0 = mean(abs(y[(t0+1):(t0+ahead)] - pred0))
   
-  #calls select_start to determine the starts of the training sets
-  sel = select_start(d,t0,min_size,test_size,window,thresh,type)
- #  sel = list()
-#  sel[[1]]=200
-#  sel[[2]]=200
-#  sel[[3]]=200
+  sel = select_start_24(user,h2,t0,min_size,test_size,window,thresh)
   
   if(sel[[1]]==1)
     lam1 = lam0
@@ -142,18 +202,39 @@ compare_methods = function(d,t0,min_size,test_size,ahead,window,thresh,type){
   pred4 = mean(y[sel[[3]]:t0])
   er4 = mean(abs(y[(t0+1):(t0+ahead)] - pred4))
   
-  return(c(er0,er1,er3,er2))
+  return(c(er0,er1,er2,er3,er4))
   
 }
 
-get_train_res = function(d,t0){
-  x = d[,1:100]
-  y = d[,101] 
-  lam0 = lambda_cv(x[1:t0,],y[1:t0])
-  mdl0 = glmnet(x[1:t0,],y[1:t0],lambda=c(lam0),alpha=0.5)
-  pred0 = predict(mdl0,x[1:t0,])
-  res = y[1:t0] - pred0
-  return(res)
+compare_many = function(start,end,min_size,test_size,ahead,window,thresh,path){
+  tried = c()
+  ers   = c()
+  users = get_data(path)
+  for(u in start:end){
+    if(monthly_filter(users[[u]]))
+      for( t in c(80,120,160,190,200,230,240,230,270,280,310,320,340)){
+        
+        part = c()
+        #h = sample(6:24,1)
+        
+        print(paste(u,t,sep=" "))
+        res = tryCatch(compare_methods(users[[u]],7,t,min_size,test_size,ahead,window,thresh),error=function(err){
+          print(err)
+          return(-1)
+        })
+        
+        if(res==-1)
+          next
+        
+        tried = c(tried,u,t)
+        ers   = c(ers,res)
+        print(res)
+      
+    }
+  }
+  
+ res = list(matrix(ers,nrow=5),matrix(tried,nrow=2))
+ save(res,file=paste("res-",min_size,test_size,ahead,window,thresh,".RData",sep=""))
 }
 
 min_moving_median = function(matrix,w){
@@ -165,67 +246,89 @@ min_moving_median = function(matrix,w){
     if(med<min){
       min = med
       index = i + (which(vector[i:(i+w)]==med))
+      #print(i)
     }
   }
   return(list(index,min))
 }
 
-#method that performs the test
-test=function(data,min_train_size=10,val_size=25,points_ahead=10,window=10,thresh=0.2,type='stat'){
-  ers = c()
-  times = c()
-  for(i in 1:length(synthetic_data)){
-    print(i)
-    d = data[[i]]
-    for(t in c(80,120,160,200,240,280,320)){
-      res = tryCatch(compare_methods(d,t,min_train_size,val_size,points_ahead,window,thresh,type),error=function(err){
-       return(-1)
-      })
-      if (length(res)==1)
-        next
-      print(res)
-      ers = c(ers, res)
-      times = c(times , t)
+parse_results = function(res,t0){
+  hours = c()
+  ids = c()
+  for(i in 1:dim(res[[1]])[2]){
+    if(res[[2]][2,i]==t0){
+      hours = c(hours,res[[1]][,i])
+      ids = c(ids,res[[2]][,i] )
     }
   }
-  results = list(matrix(ers,nrow=4),times)
-  print(parse_all(results))
-  return(results)
+  ers = matrix(hours,nrow=5)
+  ids = matrix(ids, nrow=2)
+  norm = mean(ers[1,])
+  r = c()
+  r = c(r,mean((ers[1,]-ers[2,])/ers[1,]))
+  r = c(r,mean((ers[1,]-ers[3,])/ers[1,]))
+  r = c(r,mean((ers[1,]-ers[4,])/ers[1,]))
+  r = c(r,mean((ers[1,]-ers[5,])/ers[1,]))
+  
+  r = c(r,mean((ers[1,]-ers[2,])/norm))
+  r = c(r,mean((ers[1,]-ers[3,])/norm))
+  r = c(r,mean((ers[1,]-ers[4,])/norm))
+  r = c(r,mean((ers[1,]-ers[5,])/norm))
+  
+  return(r)
 }
 
 parse_all = function(res){
-  ers = matrix(0,3,8)
-  times = c(80,120,160,200,240,280,320)
-  for( i in 1:7){
-    for(j in 1:3){
-      r = res[[1]][,res[[2]]==times[i]]
-      ers[j,i] = mean(r[1,]-r[(j+1),])/mean(r[1,])
-    }
-  }
-  for(i in 1:3)
-    ers[i,8] = mean(ers[i,1:7])
-  
-  return(ers*100)
+  ers = array(dim=c(8,8))
+ t0 = c(80,120,160,200,240,280,320,340)
+ #t0 = c(200,240,280,320)
+ for( j in t0  )
+    ers[,which(t0==j)] = parse_results(res,j)
+ print(mean(ers[1,]))
+ print(mean(ers[5,]))
+  return(ers)  
 }
 
-lambda_cv = function(x,y,k=5){
-  len       = length(y)
-  fold_size =  floor(len/k)
-  ers = c()
-  lam = glmnet(x,y,nlambda=20,alpha=0.5)$lambda
-  for(i in 0:(k-1)){
-    test_ind = (i*fold_size+1):(i*fold_size+fold_size)
-    train_x  = x[-test_ind,]
-    train_y  = y[-test_ind]
-    test_x   = x[test_ind,]
-    test_y   = y[test_ind]
-    mdl  = glmnet(train_x,train_y,lambda=lam,alpha=0.5)
-    pred = predict(mdl,test_x,nlambda=lam)
-    buf  = sqrt(colSums((pred-test_y)**2)/fold_size)
-    if(length(buf)==length(lam))
-      ers = c(ers,buf)
-  }
-  er_mat = matrix(ers,nrow=length(lam))
-  sums   = rowSums(er_mat)
-  return(mdl$lambda[which(sums==min(sums))])
+remove_period = function(user,h2,t0,t1,t2,ahead=10){
+  dt = data.matrix(make_all_dataset_truth(user,h2))
+  d = rbind(dt[1:t1,],dt[t2:t0,])
+  x = d[,1:31]
+  y = d[,32] 
+  
+  lam0 = lambda_cv(x,y)
+  mdl0 = glmnet(x,y,lambda=c(lam0),alpha=0.5)
+  pred0 = predict(mdl0,dt[(t0+1):(t0+ahead),1:31])
+  er0 = mean(abs(dt[(t0+1):(t0+ahead),32] - pred0))
+  
+   print(er0)
 }
+
+gradients = function(user,h2,t1,t0){
+  d = data.matrix(make_all_dataset_truth(user,h2))
+  x = d[,1:31]
+  y = d[,32] 
+  if(any(y>10000)||length(y)>10000)
+    return(-1)
+  lam0 = lambda_cv(x[t1:t0,],y[t1:t0])
+  mdl0 = glmnet(x[t1:t0,],y[t1:t0],lambda=c(lam0),alpha=0.5)
+  
+  grads = matrix(0,t0-t1,32)
+  preds = predict(mdl0,x[t1:t0,])
+  
+  for(i in 1:(t0-t1)){
+    for( j in 1:31)
+      grads[i,j] = -(preds[i]-y[i])*x[i,j]
+    grads[i,32] = -(preds[i]-y[i])
+  }
+  
+  for(i in 1:31){
+    plot(grads[,i],sub=i)
+    title(paste(mdl0$beta[i],mean(x[t1:t0,i]),sep=" "))
+    
+    #  points(y**2,col='red')
+  }
+  plot(grads[,32],sub="a0")
+  title(mdl0$a0)
+  return(grads)
+}
+
