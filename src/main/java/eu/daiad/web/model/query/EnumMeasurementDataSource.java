0ed8b91bcd61a513c04e0f7ca669f1e0242f271d
Yannis Kouvaras <yannis.kouvaras@kupa.gr>
2016-09-05 07:12:44 +0300

    Temporary fix for generating trial reports

--- src/main/java/eu/daiad/web/model/query/EnumMeasurementDataSource.java
+++ src/main/java/eu/daiad/web/model/query/EnumMeasurementDataSource.java
@@ -10,47 +10,60 @@ import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 
 public enum EnumMeasurementDataSource {
-	NONE(0), BOTH(1), AMPHIRO(2), METER(3);
+    NONE(0), BOTH(1), AMPHIRO(2), METER(3), DEVICE(4);
 
-	private final int value;
+    private final int value;
 
-	private EnumMeasurementDataSource(int value) {
-		this.value = value;
-	}
+    private EnumMeasurementDataSource(int value) {
+        this.value = value;
+    }
 
-	public int getValue() {
-		return this.value;
-	}
+    public int getValue() {
+        return this.value;
+    }
 
-	private static final Map<Integer, EnumMeasurementDataSource> intToTypeMap = new HashMap<Integer, EnumMeasurementDataSource>();
-	static {
-		for (EnumMeasurementDataSource type : EnumMeasurementDataSource.values()) {
-			intToTypeMap.put(type.value, type);
-		}
-	}
+    private static final Map<Integer, EnumMeasurementDataSource> intToTypeMap = new HashMap<Integer, EnumMeasurementDataSource>();
+    static {
+        for (EnumMeasurementDataSource type : EnumMeasurementDataSource.values()) {
+            intToTypeMap.put(type.value, type);
+        }
+    }
 
-	public static EnumMeasurementDataSource fromInteger(int value) {
-		EnumMeasurementDataSource type = intToTypeMap.get(Integer.valueOf(value));
-		if (type == null)
-			return EnumMeasurementDataSource.NONE;
-		return type;
-	}
+    public static EnumMeasurementDataSource fromInteger(int value) {
+        EnumMeasurementDataSource type = intToTypeMap.get(Integer.valueOf(value));
+        // Temporary fix - start
+        if (type == DEVICE) {
+            return AMPHIRO;
+        }
+        // Temporary fix - end
 
-	public static EnumMeasurementDataSource fromString(String value) {
-		for (EnumMeasurementDataSource item : EnumMeasurementDataSource.values()) {
-			if (item.name().equalsIgnoreCase(value)) {
-				return item;
-			}
-		}
-		return EnumMeasurementDataSource.NONE;
-	}
+        if (type == null) {
+            return EnumMeasurementDataSource.NONE;
+        }
 
-	public static class Deserializer extends JsonDeserializer<EnumMeasurementDataSource> {
+        return type;
+    }
 
-		@Override
-		public EnumMeasurementDataSource deserialize(JsonParser parser, DeserializationContext context)
-						throws IOException, JsonProcessingException {
-			return EnumMeasurementDataSource.fromString(parser.getValueAsString());
-		}
-	}
+    public static EnumMeasurementDataSource fromString(String value) {
+        for (EnumMeasurementDataSource item : EnumMeasurementDataSource.values()) {
+            if (item.name().equalsIgnoreCase(value)) {
+                // Temporary fix - start
+                if (item.name().equalsIgnoreCase("DEVICE")) {
+                    return AMPHIRO;
+                }
+                // Temporary fix - end
+                return item;
+            }
+        }
+        return EnumMeasurementDataSource.NONE;
+    }
+
+    public static class Deserializer extends JsonDeserializer<EnumMeasurementDataSource> {
+
+        @Override
+        public EnumMeasurementDataSource deserialize(JsonParser parser, DeserializationContext context)
+                        throws IOException, JsonProcessingException {
+            return EnumMeasurementDataSource.fromString(parser.getValueAsString());
+        }
+    }
 }
