752a6c47d3257b52167b22becdcc69d98e448b71
nkaragiannakis <karagiannakis.nk@gmail.com>
2016-09-08 21:28:18 +0300

    basic UI for Favourites section

---
+++ src/main/resources/public/assets/js/src/utility/constants/FavouritesActionTypes.js
@@ -0,0 +1,11 @@
+var mirrorToPath = require('../helpers/path-mirror.js');
+
+var types = mirrorToPath({
+  FAVOURITES_FAVOURITES_REQUEST : null,
+  FAVOURITES_FAVOURITES_RESPONSE : null,
+  FAVOURITES_OPEN_SELECTED : null,
+  FAVOURITES_CLOSE_SELECTED : null  
+  
+});
+
+module.exports = types;
