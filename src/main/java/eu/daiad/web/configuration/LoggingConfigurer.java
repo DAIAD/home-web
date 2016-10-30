908123055d348aa4cabbbfa52f61d3c7802026a7
Yannis Kouvaras <yannis.kouvaras@kupa.gr>
2016-10-30 13:22:50 +0200

    Update logging configuration
    Replaced deprecated method call after Spring updated the Apache Log4j2 library.
    Updated code formatting. Replaced tabs with spaces.
    Added comments to the code.

--- src/main/java/eu/daiad/web/configuration/LoggingConfigurer.java
+++ src/main/java/eu/daiad/web/configuration/LoggingConfigurer.java
@@ -22,73 +22,132 @@ import eu.daiad.web.logging.MappedDiagnosticContextKeys;
 
 /**
  * Configures application logging system.
- *
  */
 @Component
 public class LoggingConfigurer implements InitializingBean {
 
-	@Value("${log4j2.logger.jdbc.name:eu.daiad}")
-	private String loggerName;
+    /**
+     * Name of Log4j2 JDBC logger.
+     */
+    @Value("${log4j2.logger.jdbc.name:eu.daiad}")
+    private String loggerName;
 
-	@Value("${log4j2.appender.jdbc.name:PostgreSQL}")
-	private String appenderName;
+    /**
+     * Name of Log4j2 JDBC appender.
+     */
+    @Value("${log4j2.appender.jdbc.name:PostgreSQL}")
+    private String appenderName;
 
-	@Value("${log4j2.appender.jdbc.table:log4j_message}")
-	private String tableName;
+    /**
+     * Table name used by the configured Log4j2 JDBC appender.
+     */
+    @Value("${log4j2.appender.jdbc.table:log4j_message}")
+    private String tableName;
 
-	@Value("${log4j2.appender.jdbc.categories:}")
-	private String categories;
+    /**
+     * Error categories logged by the Log4j2 JDBC appender. If no categories are
+     * set, all events are logged.
+     */
+    @Value("${log4j2.appender.jdbc.categories:}")
+    private String categories;
 
-	@Autowired
-	ApplicationContext applicationContext;
+    /**
+     * Spring application context.
+     */
+    @Autowired
+    ApplicationContext applicationContext;
 
-	/**
-	 * Adds a {@link JdbcAppender} to the logging system configuration.
-	 */
-	@Override
-	public void afterPropertiesSet() throws Exception {
-		ConnectionSource connectionSource = applicationContext.getBean(ConnectionSource.class);
+    /**
+     * Adds a {@link JdbcAppender} to the logging system configuration.
+     */
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        ConnectionSource connectionSource = applicationContext.getBean(ConnectionSource.class);
 
-		// Get Log4j2 configuration
-		final LoggerContext loggerContext = (LoggerContext) LogManager.getContext(false);
-		final Configuration config = loggerContext.getConfiguration();
+        // Get Log4j2 configuration
+        final LoggerContext loggerContext = (LoggerContext) LogManager.getContext(false);
+        final Configuration config = loggerContext.getConfiguration();
 
-		// Declare table columns
-		ColumnConfig[] columnConfigs = {
-						ColumnConfig.createColumnConfig(config, "account", "%X{" + MappedDiagnosticContextKeys.USERNAME
-										+ "}", null, null, "false", null),
-						ColumnConfig.createColumnConfig(config, "remote_address", "%X{"
-										+ MappedDiagnosticContextKeys.IP_ADDRESS + "}", null, null, "false", null),
-						ColumnConfig.createColumnConfig(config, "category", "%X{"
-										+ MappedDiagnosticContextKeys.ERROR_CATEGORY + "}", null, null, "false", null),
-						ColumnConfig.createColumnConfig(config, "code", "%X{" + MappedDiagnosticContextKeys.ERROR_CODE
-										+ "}", null, null, "false", null),
-						ColumnConfig.createColumnConfig(config, "level", "%level", null, null, "false", null),
-						ColumnConfig.createColumnConfig(config, "logger", "%logger", null, null, "false", null),
-						ColumnConfig.createColumnConfig(config, "message", "%message", null, null, "false", null),
-						ColumnConfig.createColumnConfig(config, "exception", "%rEx{full}", null, null, "false", null),
-						ColumnConfig.createColumnConfig(config, "timestamp", null, null, "true", null, null) };
+        // Declare table columns
+        ColumnConfig[] columnConfigs = {
+            // Account name
+            ColumnConfig.createColumnConfig(config,
+                                            "account",
+                                            "%X{" + MappedDiagnosticContextKeys.USERNAME + "}",
+                                            null,
+                                            null,
+                                            "false",
+                                            null),
+            // Remote address
+            ColumnConfig.createColumnConfig(config,
+                                            "remote_address", "%X{" + MappedDiagnosticContextKeys.IP_ADDRESS + "}",
+                                            null,
+                                            null,
+                                            "false",
+                                            null),
+            // Error category
+            ColumnConfig.createColumnConfig(config,
+                                            "category",
+                                            "%X{" + MappedDiagnosticContextKeys.ERROR_CATEGORY + "}",
+                                            null,
+                                            null,
+                                            "false",
+                                            null),
+            // Error code
+            ColumnConfig.createColumnConfig(config,
+                                            "code",
+                                            "%X{" + MappedDiagnosticContextKeys.ERROR_CODE + "}",
+                                            null,
+                                            null,
+                                            "false",
+                                            null),
+            // Level
+            ColumnConfig.createColumnConfig(config, "level",     "%level",     null, null,  "false", null),
+            // Logger
+            ColumnConfig.createColumnConfig(config, "logger",    "%logger",    null, null,  "false", null),
+            // Message
+            ColumnConfig.createColumnConfig(config, "message",   "%message",   null, null,  "false", null),
+            // Exception
+            ColumnConfig.createColumnConfig(config, "exception", "%rEx{full}", null, null,  "false", null),
+            // Timestamp with reference to the server local time zone
+            ColumnConfig.createColumnConfig(config, "timestamp",  null,        null, "true", null, null) };
 
-		// Create Filter
-		Filter filter = ErrorCodeFilter.createFilter(categories);
+        // Create Filter
+        Filter filter = ErrorCodeFilter.createFilter(categories);
 
-		// Create JDBC appender
-		Appender appender = JdbcAppender.createAppender(appenderName, null, filter, connectionSource, null, tableName,
-						columnConfigs);
+        // Create JDBC appender
+        Appender appender = JdbcAppender.createAppender(appenderName,
+                                                        null,
+                                                        filter,
+                                                        connectionSource,
+                                                        null,
+                                                        tableName,
+                                                        columnConfigs);
 
-		appender.start();
-		config.addAppender(appender);
+        appender.start();
+        config.addAppender(appender);
 
-		// Add logger
-		AppenderRef ref = AppenderRef.createAppenderRef(appenderName, null, null);
-		AppenderRef[] refs = new AppenderRef[] { ref };
-		LoggerConfig loggerConfig = LoggerConfig.createLogger("true", Level.INFO, loggerName, null, refs, null, config,
-						null);
-		loggerConfig.addAppender(appender, null, null);
+        // Create appender reference
+        AppenderRef ref = AppenderRef.createAppenderRef(appenderName, null, null);
+        AppenderRef[] refs = new AppenderRef[] { ref };
 
-		config.addLogger(loggerName, loggerConfig);
+        // Create logger
+        LoggerConfig loggerConfig = LoggerConfig.createLogger(true,
+                                                              Level.INFO,
+                                                              loggerName,
+                                                              null,
+                                                              refs,
+                                                              null,
+                                                              config,
+                                                              null);
 
-		// Refresh context
-		loggerContext.updateLoggers();
-	}
+        // Add the JDBC appender to the logger configuration and the logger to
+        // the logging configuration
+        loggerConfig.addAppender(appender, null, null);
+
+        config.addLogger(loggerName, loggerConfig);
+
+        // Refresh context
+        loggerContext.updateLoggers();
+    }
 }
