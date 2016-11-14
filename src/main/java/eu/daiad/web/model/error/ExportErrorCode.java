b67b7d93f9f9d127c410ed8b6de5e1fbde1cec4f
Yannis Kouvaras <yannis.kouvaras@kupa.gr>
2016-11-14 20:53:40 +0200

    Update messages for data export job
    Validation rule messages for data export job have been moved to the resource file messages.properties.

---
+++ src/main/java/eu/daiad/web/model/error/ExportErrorCode.java
@@ -0,0 +1,14 @@
+package eu.daiad.web.model.error;
+
+public enum ExportErrorCode implements ErrorCode {
+    VALIDATION_RULE_VOLUME_LESS,
+    VALIDATION_RULE_DURATION_LESS,
+    VALIDATION_RULE_FLOW_LESS,
+    VALIDATION_RULE_VOLUME_PERCENTILE,
+    VALIDATION_RULE_TOO_MANY_RECORDS_REMOVED;
+
+    @Override
+    public String getMessageKey() {
+        return (this.getClass().getSimpleName() + '.' + name());
+    }
+}
