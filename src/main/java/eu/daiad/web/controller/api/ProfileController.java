108b6898efced4d84380c26313aac8c46821d91c
Yannis Kouvaras <yannis.kouvaras@kupa.gr>
2017-04-06 14:50:57 +0300

    Allow querying data for other users
    Updated profile and search controllers for searching shower and comparison/ranking data for other users except for the authenticated user.

--- src/main/java/eu/daiad/web/controller/api/ProfileController.java
+++ src/main/java/eu/daiad/web/controller/api/ProfileController.java
@@ -2,6 +2,7 @@ package eu.daiad.web.controller.api;
 
 import java.util.Map;
 import java.util.Set;
+import java.util.UUID;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
@@ -32,6 +33,7 @@ import eu.daiad.web.model.security.AuthenticatedUser;
 import eu.daiad.web.model.security.Credentials;
 import eu.daiad.web.model.security.EnumRole;
 import eu.daiad.web.repository.application.IProfileRepository;
+import eu.daiad.web.repository.application.IUserRepository;
 import eu.daiad.web.repository.application.IWaterIqRepository;
 import eu.daiad.web.util.ValidationUtils;
 
@@ -45,6 +47,12 @@ public class ProfileController extends BaseRestController {
      * Logger instance for writing events using the configured logging API.
      */
     private static final Log logger = LogFactory.getLog(ProfileController.class);
+
+    /**
+     * Repository for accessing user data.
+     */
+    @Autowired
+    private IUserRepository userRepository;
 
     /**
      * Repository for accessing user profile data.
@@ -115,6 +123,47 @@ public class ProfileController extends BaseRestController {
                 throw createApplicationException(SharedErrorCode.AUTHORIZATION);
             }
 
+        } catch (Exception ex) {
+            logger.error(ex.getMessage(), ex);
+
+            return new RestResponse(getError(ex));
+        }
+    }
+
+    /**
+     * Loads comparison and ranking data for a user.
+     *
+     * @param data user credentials.
+     * @param year reference year.
+     * @param month reference month.
+     * @return the user profile.
+     */
+    @RequestMapping(value = "/api/v1/comparison/{year}/{month}/{userKey}", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
+    public RestResponse getComparisonRanking(@RequestBody Credentials data,
+                                             @PathVariable int year, @PathVariable int month,
+                                             @PathVariable UUID userKey) {
+        try {
+            AuthenticatedUser user = authenticate(data);
+
+            // If user has not administrative permissions and requests data for another user, throw an exception
+            if ((!user.hasRole(EnumRole.ROLE_SYSTEM_ADMIN, EnumRole.ROLE_UTILITY_ADMIN)) && (!user.getKey().equals(userKey))) {
+                throw createApplicationException(SharedErrorCode.AUTHORIZATION);
+            }
+
+            // Check utility access
+            if (!user.getKey().equals(userKey)) {
+                AuthenticatedUser dataOwner = userRepository.getUserByKey(userKey);
+
+                if (!user.getUtilities().contains(dataOwner.getUtilityId())) {
+                    throw createApplicationException(SharedErrorCode.AUTHORIZATION_UTILITY_ACCESS_DENIED);
+                }
+            }
+
+            ComparisonRankingResponse response = new ComparisonRankingResponse();
+
+            response.setComparison(waterIqRepository.getWaterIqByUserKey(userKey, year, month));
+
+            return response;
         } catch (Exception ex) {
             logger.error(ex.getMessage(), ex);
 
